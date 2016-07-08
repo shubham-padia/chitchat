7504be090fb7eb5509f06664d3721d76aee53092
shubham-padia <shubhamapadia@gmail.com>
2016-07-08 15:43:08 +0530

    functionality to add and delete rooms added

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 var express = require('express');
 var app = express();
-var rooms = require("./data/rooms.json");
+var rooms = require('./data/rooms.json');
+var bodyparser = require('body-parser');
+var uuid = require('node-uuid');
 
 app.set('views','./views');
 app.set('view engine','jade')
 
 app.use(express.static('public'));
 app.use(express.static('node_modules/bootstrap/dist'));
+app.use(bodyparser.urlencoded({extended : true}));
 
 app.get('/admin/rooms',function(req,res){
         res.render('rooms',{title: "Rooms", rooms: rooms});
+});
+app.get('/admin/rooms/add',function(req,res){
+        res.render('add');
+});
+app.post('/admin/rooms/add',function(req,res){
+        var room = {
+                name: req.body.room_name,
+                id: uuid.v4()
+        }
+
+        rooms.push(room);
+        res.redirect('/admin/rooms');
+});
+app.get('/admin/rooms/delete/:id',function(req,res){
+        var room_id = req.params.id;
+        rooms = rooms.filter( a => a.id != room_id);
+        res.redirect('/admin/rooms');
 });
 app.get('/admin/users',function(req,res){
         res.render('users',{title: "Users"});
